d7801c799077057f87054f0b2e0c5efb46933922
radhika <rvramayra1998@gmail.com>
2022-05-17 21:12:02 +0530

    counter

---
+++ counter/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Counter } from './Components/counter';
+
+function App() {
+  return (
+    <div className="App">
+ <Counter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ counter/src/Components/counter.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./counter.css"
+export const Counter=()=>{
+    const [count,setCount] = useState(0);
+    const handleCount=(value)=>{
+        setCount(value+count)
+    
+    }
+    const handleCountByTwo=(value)=>{
+        setCount(value*count)
+    }
+    return (
+        <div>
+            <h1 className={`${count%2===0? "even":"odd"}`}>Counter:{count}</h1>
+            <button onClick={()=>{
+                handleCount(1)
+            }}>Add</button>
+            <button onClick={()=>{
+                if(count>0){
+                    handleCount(-1)
+                }}}>Subb</button>
+<button onClick={()=>{
+                handleCountByTwo(2)
+                }}>duble</button>
+                <h1 className={`${count%2===0? "even":"odd"}`}  >{count%2===0 ? "Even No.": "Odd No."}
+                </h1>
+        </div>
+    )
+}
